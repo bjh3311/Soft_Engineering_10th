31d2f8f24205e4c631a5ca3fb00e49ee1e522df3
Ungs <50164778+iqeq1945@users.noreply.github.com>
2021-05-16 19:24:22 +0900

    관리자페이지 접근 불가 시스템

--- routes/admin.js
+++ routes/admin.js
@@ -2,60 +2,61 @@
 
 var express = require('express');
 var router = express.Router();
+var util = require('../util');
 var Product = require('../models/Product');
 var Post = require('../models/Post');
 
 // Index
-router.get('/index', function(req, res){
+router.get('/index', util.isLoggedin, checkPermission,  function(req, res){
     res.render('admin/index');
 });
-router.get('/table', async function(req, res){
+router.get('/table', util.isLoggedin, checkPermission,  async function(req, res){
   posts = await Post.find();
     res.render('admin/table', {
     posts:posts
   });
 });
 
-router.get('/table_delete',function(req,res){
+router.get('/table_delete', util.isLoggedin, checkPermission, function(req,res){
   res.render('admin/table_delete');
 });
 
-router.get('/table_detail',function(req,res){
+router.get('/table_detail',util.isLoggedin, checkPermission, function(req,res){
   res.render('admin/table_detail')
 })
 
-router.get('/form_create', function(req, res){
+router.get('/form_create', util.isLoggedin, checkPermission, function(req, res){
     res.render('admin/form_create');
 });
 
-router.get('/form_update', function(req, res){
+router.get('/form_update', util.isLoggedin, checkPermission, function(req, res){
     res.render('admin/form_update');
 });
 
-router.get('/register', function(req, res){
+router.get('/register', util.isLoggedin, checkPermission, function(req, res){
     res.render('admin/register');
 });
 
-router.get('/detail', function(req, res){
+router.get('/detail', util.isLoggedin, checkPermission, function(req, res){
   res.render('admin/detail');
 });
-router.get('/modify', function(req, res){
+router.get('/modify',util.isLoggedin, checkPermission,  function(req, res){
     res.render('admin/modify');
 });
 
 // 주문 내역
-router.get('/order_list',function(req,res){
+router.get('/order_list',util.isLoggedin, checkPermission, function(req,res){
   res.render('admin/order_list');
 });
 
 // 판매 정보 통계
-router.get('/sales_statistics',function(req,res){
+router.get('/sales_statistics',util.isLoggedin, checkPermission, function(req,res){
   res.render('admin/sales_statistics');
 });
 
 // 상품 관리
 
-router.get('/shop', async function(req,res){
+router.get('/shop',util.isLoggedin, checkPermission,  async function(req,res){
   products = await Product.find();
     res.render('admin/shop', {
     products:products
@@ -63,3 +64,8 @@ router.get('/shop', async function(req,res){
 });
 
   module.exports = router;
+
+  function checkPermission(req, res, next){
+    if(req.user.right == false) return util.noPermission(req,res);
+    next();
+  }
--- util.js
+++ util.js
@@ -23,4 +23,20 @@ util.parseError = function(errors){
   return parsed;
 }
 
+util.isLoggedin = function(req, res, next){
+  if(req.isAuthenticated()){
+    next();
+  } 
+  else {
+    req.flash('errors', {login:'Please login first'});
+    res.redirect('/');
+  }
+}
+
+util.noPermission = function(req, res){
+  req.flash('errors', {login:"You don't have permission"});
+  req.logout();
+  res.redirect('/');
+}
+
 module.exports = util;
