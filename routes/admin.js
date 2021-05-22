e6aece917f826512b415a17a992784a261572b0f
Ungs <50164778+iqeq1945@users.noreply.github.com>
2021-05-22 12:57:42 +0900

    search system update

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,7 @@ var router = express.Router();
 var util = require('../util');
 var Product = require('../models/Product');
 var Post = require('../models/Post');
+const { route } = require('./main');
 
 
 // Index
@@ -58,15 +59,55 @@ router.get('/sales_statistics',util.isLoggedin, checkPermission, function(req,re
 // 상품 관리
 
 router.get('/shop',util.isLoggedin, checkPermission,  async function(req,res){
-  products = await Product.find();
-    res.render('admin/shop', {
-    products:products
+  var origin = Math.max(0,parseInt(req.query.origin));
+  origin =!isNaN(origin)?origin:0;                    
+
+  var searchQuery = createSearchQuery(req.query);
+  //console.log(searchQuery);
+ 
+  if(origin == 0){
+  var products = await Product.find(searchQuery)
+    .sort('-createdAt')
+    .exec();
+  }else{
+    var products = await Product.find({'origin' : origin},searchQuery)
+    .sort('-createdAt')
+    .exec();
+  }
+  res.render('admin/shop', {
+    products:products,
+    origin:origin,
+    searchText: req.query.searchText
   });
 });
+
+router.get('/test',util.isLoggedin, checkPermission, function(req,res){
+  console.log(req.body);
+  res.redirect('/admin/shop');
+})
+
+
+
 
 module.exports = router;
 
 function checkPermission(req, res, next){
   if(req.user.right == false) return util.noPermission(req,res);
   next();
+}
+
+function createSearchQuery(queries){ // 4
+  var searchQuery = {};
+  if(queries.searchText && queries.searchText.length >= 2){
+    //var searchTypes = queries.searchType.toLowerCase().split(',');
+    var productQueries = [];
+    productQueries.push({ name: { $regex: new RegExp(queries.searchText, 'i') } });
+    
+    /*
+    if(searchTypes.indexOf('body')>=0){
+      productQueries.push({ body: { $regex: new RegExp(queries.searchText, 'i') } });
+    }*/
+    if(productQueries.length > 0) searchQuery = {$or:productQueries};
+  }
+  return searchQuery;
 }
