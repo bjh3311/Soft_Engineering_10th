d9ddb9a82ec4fcbd77ec8a015ef2542905e7d006
Hanalin <Hanalin0422@gmail.com>
2021-05-30 13:24:27 +0900

    일단 넣고 보자

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ app.use('/admin', require('./routes/admin'));
 app.use('/users', require('./routes/users'));
 app.use('/products', require('./routes/products'));
 app.use('/posts', require('./routes/posts'));
+app.use('/destination', require('./routes/destination'));
 
 
 // port setting
